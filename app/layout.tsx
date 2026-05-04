0a9b45825c212690cc5c8e490210cec579521060
Aaryan Kunjan <aaryan@example.com>
2026-05-05 03:38:17 +0530

    Add Google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export const metadata: Metadata = {
       "max-video-preview": -1,
     },
   },
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
+  },
 };
 
 export default function RootLayout({
